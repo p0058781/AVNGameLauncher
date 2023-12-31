ad4a13cf91f9e5bc6da664f36b73378d2e53533e
pedja <pedja1@mailfence.com>
2023-12-31 14:12:49 +0100

    use gradle catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,29 +11,6 @@ pluginManagement {
         google()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    plugins {
-        val kotlinVersion = extra["kotlin.version"] as String
-        val agpVersion = extra["agp.version"] as String
-        val composeVersion = extra["compose.version"] as String
-        val realmVersion = extra["realm.version"] as String
-
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("plugin.serialization").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
-
-
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-
-        id("org.jetbrains.compose").version(composeVersion)
-
-        id("io.realm.kotlin").version(realmVersion)
-        id("io.ktor.plugin").version("2.3.7")
-        id("org.jlleitschuh.gradle.ktlint").version("12.0.3")
-        //TODO use version catalog
-    }
 }
 
 plugins {
