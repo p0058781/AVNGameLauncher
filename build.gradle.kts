de3b951ea6cd4e6ac3e06b00b19d951320743b21
p0058781 <p0058781@gmail.com>
2024-03-12 17:08:04 +0100

    use gradle catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    kotlin("multiplatform").apply(false)
-    id("com.android.application").apply(false)
-    id("com.android.library").apply(false)
-    id("org.jetbrains.compose").apply(false)
-    id("io.realm.kotlin").apply(false)
-    id("org.jlleitschuh.gradle.ktlint").apply(false)
+    alias(libs.plugins.kotlin.jvm).apply(false)
+    alias(libs.plugins.kotlin.multiplatform).apply(false)
+    alias(libs.plugins.kotlin.serialization).apply(false)
+    alias(libs.plugins.kotlin.android).apply(false)
+
+
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.android.library).apply(false)
+
+    alias(libs.plugins.jetbrains.compose).apply(false)
+
+    alias(libs.plugins.realm.kotlin).apply(false)
+    alias(libs.plugins.ktor.plugin).apply(false)
+    alias(libs.plugins.ktlint.plugin).apply(false)
 }
 
 subprojects {
