a33f12cb1c6409f3518db17c822711851917579f
p0058781 <p0058781@gmail.com>
2024-03-12 17:08:04 +0100

    Cleanup

--- shared/src/commonMain/kotlin/org/skynetsoftware/avnlauncher/f95/F95Api.kt
+++ shared/src/commonMain/kotlin/org/skynetsoftware/avnlauncher/f95/F95Api.kt
@@ -1,6 +1,5 @@
 package org.skynetsoftware.avnlauncher.f95
 
-import com.russhwolf.settings.Settings
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.logging.LogLevel
@@ -13,17 +12,12 @@ import org.skynetsoftware.avnlauncher.f95.model.F95Game
 const val F95_ZONE_THREAD_BASE_URL = "https://f95zone.to/threads/"
 
 val f95ApiKoinModule = module {
-    single<F95Api> { F95ApiImpl(get(), F95CookiesStorage(Settings())) }
+    single<F95Api> { F95ApiImpl(get()) }
 }
 
 fun Int.createF95ThreadUrl() = "$F95_ZONE_THREAD_BASE_URL$this"
 
 interface F95Api {
-    suspend fun login(
-        username: String,
-        password: String,
-    ): Result<Unit>
-
     suspend fun getGame(gameThreadId: Int): Result<F95Game>
 
     suspend fun getGame(gameThreadUrl: String): Result<F95Game>
@@ -31,13 +25,9 @@ interface F95Api {
     suspend fun getRedirectUrl(gameThreadId: Int): Result<String>
 }
 
-private class F95ApiImpl(private val f95Parser: F95Parser, private val f95CookiesStorage: F95CookiesStorage) : F95Api {
+private class F95ApiImpl(private val f95Parser: F95Parser) : F95Api {
     private val gameThreadUrlRegex = Regex("https://f95zone.to/threads/.+\\.(\\d+)")
-    private val loginPageUrl = "https://f95zone.to/login/"
     private val httpClient = HttpClient {
-        /*install(HttpCookies) {
-            storage = f95CookiesStorage
-        }*/
         install(HttpTimeout) {
             requestTimeoutMillis = 60000
         }
@@ -47,13 +37,6 @@ private class F95ApiImpl(private val f95Parser: F95Parser, private val f95Cookie
     }
     private val noRedirectClient = httpClient.config {
         followRedirects = false
-    }
-
-    override suspend fun login(
-        username: String,
-        password: String,
-    ): Result<Unit> {
-        TODO("not implemented")
     }
 
     override suspend fun getGame(gameThreadId: Int): Result<F95Game> {
