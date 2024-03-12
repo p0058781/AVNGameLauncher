0aa778da7800fc7aacc51cb6d0ec96beb5794f91
p0058781 <p0058781@gmail.com>
2024-03-12 17:08:07 +0100

    average play time fixes

--- sharedApp/src/commonMain/kotlin/org/skynetsoftware/avnlauncher/utils/TimeUtils.kt
+++ sharedApp/src/commonMain/kotlin/org/skynetsoftware/avnlauncher/utils/TimeUtils.kt
@@ -2,11 +2,14 @@ package org.skynetsoftware.avnlauncher.utils
 
 import androidx.compose.runtime.Composable
 import dev.icerock.moko.resources.compose.stringResource
+import kotlinx.datetime.Clock
 import org.skynetsoftware.avnlauncher.MR
 
 private const val ONE_SECOND_MILLIS = 1000L
 private const val ONE_HOUR_SECONDS = 3600L
 private const val ONE_MINUTE_SECONDS = 60L
+private const val HOURS_IN_DAY = 24f
+private const val ONE_DAY_MS = 86400000f
 
 @Composable
 fun formatPlayTime(playTime: Long?): String {
@@ -31,4 +34,15 @@ fun formatPlayTime(playTime: Long?): String {
 fun Int?.hoursToMilliseconds(): Long? {
     this ?: return null
     return this * ONE_HOUR_SECONDS * ONE_SECOND_MILLIS
+}
+
+fun calculateAveragePlayTime(
+    firstPlayedTime: Long,
+    totalPlayTime: Long,
+): Float {
+    val now = Clock.System.now().toEpochMilliseconds()
+    val totalTimeDays = ((now - firstPlayedTime) / ONE_DAY_MS).let { if (it < 1f) 1f else it }
+    val totalPlayTimeDays = totalPlayTime / ONE_DAY_MS
+    val dailyPlayTimeHours = (totalPlayTimeDays / totalTimeDays) * HOURS_IN_DAY
+    return if (dailyPlayTimeHours > HOURS_IN_DAY) HOURS_IN_DAY else dailyPlayTimeHours
 }
